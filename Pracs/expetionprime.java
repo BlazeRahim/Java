e5975c0e6809875920cba0b9f69390e6ac6ba5ac
AbduRahim <77384491+BlazeRahim@users.noreply.github.com>
2022-11-15 22:40:06 +0530

    done

---
+++ Pracs/expetionprime.java
@@ -0,0 +1,64 @@
+package Pracs;
+
+import java.util.*;
+
+public class expetionprime
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int[] n = new int[5];
+        System.out.println("enter the prime numbers:");
+        for(int i = 0 ; i<5;i++){
+             n[i] = s.nextInt();
+        }
+       
+        int j=0;
+        for(int i=0; i<5; i++)
+        {
+            try
+            {
+               boolean condition = prime(n[i],false);
+               if(condition==false){
+                j++;
+                throw new oddexception("Number is non prime:"+n[i]);
+               }else{
+                   System.out.println("the square of entered prime number:" + n[i] + " is " + n[i]*n[i]);
+               }
+              
+            }
+            catch(oddexception e)
+            {
+                 e.printStackTrace();
+            }
+        }
+        System.out.println("Invalid numbers are : "+j);
+    }
+ 
+    
+
+    
+    static boolean prime(int num,boolean condition){
+           
+            int count=0;
+             for(int k=num;k>0;k--){
+                if(num%k==0){
+                   count++;
+                }
+             }
+             if(count==2){
+                condition= true;
+             }
+            return condition;
+    }
+
+}
+
+class oddexception extends Exception
+{
+    public oddexception(String message)
+    {   
+        System.out.println(message);
+    
+    }
+}
---
+++ Pracs/stringandstringbuffer.java
@@ -0,0 +1,30 @@
+package Pracs;
+
+import java.util.Scanner;
+class Palindrome{
+    Scanner sc = new Scanner(System.in);
+    String str;
+    String rev;
+    StringBuffer strl = new StringBuffer();
+    void input(){
+        System.out.println("Enter a string");
+        str=sc.nextLine();
+        strl.append(str);
+    }
+    void palindrome(){
+        strl.reverse();
+        rev=strl.toString();
+        if(str.equalsIgnoreCase(rev))
+        System.out.println(str+ " is a Palindrome");
+        else
+        System.out.println(str+ " is not a Palindrome");
+    }
+}
+
+public class stringandstringbuffer{
+    public static void main(String[] args){
+        Palindrome ob = new Palindrome();
+        ob.input();
+        ob.palindrome();
+    }
+}
